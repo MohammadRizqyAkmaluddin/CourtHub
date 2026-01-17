f0c8e38f2b232ff14cf17daa8516f30a7091986c
Mohammad Rizqy Akmaluddin <rizqy.akmaluddin@gmail.com>
2026-01-17 16:28:59 +0700

    Fixing Regional Area

--- app/Http/Controllers/Api/RegionController.php
+++ app/Http/Controllers/Api/RegionController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Region;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class RegionController extends Controller
 {
     public function index() {
         return response()->json(
-            Region::all()
+            City::all()
         );
     }
 }
