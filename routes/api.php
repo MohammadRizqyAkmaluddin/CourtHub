fc054d74282852f07a7f6eda1113d7a771d32691
Mohammad Rizqy Akmaluddin <rizqy.akmaluddin@gmail.com>
2026-01-17 16:31:05 +0700

    Update Route @Authentication

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,20 @@ use App\Http\Controllers\Api\UserAuthController;
 use App\Http\Controllers\Api\VenueAuthController;
 use App\Http\Controllers\Api\VenueController;
 
-Route::get('regions', [RegionController::class, 'index']);
-
 Route::prefix('auth/venue')->group(function () {
     Route::post('register', [VenueAuthController::class, 'register']);
     Route::post('login', [VenueAuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('logout', [VenueAuthController::class, 'logout']);
 });
 
 Route::prefix('auth/user')->group(function () {
     Route::post('register', [UserAuthController::class, 'register']);
-    Route::post('/login', [UserAuthController::class, 'login']);
+    Route::post('login', [UserAuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('logout', [UserAuthController::class, 'logout']);
 });
 
+
+Route::get('regions', [RegionController::class, 'index']);
 Route::get('venues', [VenueController::class, 'index']);
 
 Route::middleware('auth:venue')->group(function () {
