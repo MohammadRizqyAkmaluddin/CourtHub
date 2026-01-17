33196519c00b6626de65717efd992d9bbf72989d
Mohammad Rizqy Akmaluddin <rizqy.akmaluddin@gmail.com>
2026-01-17 16:28:29 +0700

    Final User Authentication Controller

--- app/Http/Controllers/Api/UserAuthController.php
+++ app/Http/Controllers/Api/UserAuthController.php
@@ -21,18 +21,28 @@ class UserAuthController extends Controller
         $data['password'] = Hash::make($data['password']);
         $user = User::create($data);
 
+        $token = $user->createToken('user-token')->plainTextToken;
+
         return response()->json([
             'message' => 'Successfully Registered',
+            'token'   => $token,
             'user'    => $user
         ], 201);
     }
 
     public function login(Request $request)
     {
+        $request->validate([
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ]);
+
         $user = User::where('email', $request->email)->first();
 
         if(!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message' => 'Wrong Credential']);
+            return response()->json([
+                'message' => 'Wrong Credential'
+            ], 401);
         }
 
         $token = $user->createToken('user-token')->plainTextToken;
@@ -44,7 +54,12 @@ class UserAuthController extends Controller
         ]);
     }
 
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
 
-
-    
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
 }
